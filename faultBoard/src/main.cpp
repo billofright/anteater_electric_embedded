9a3925955ce6e531f8c28f5066c438087766f281
Tyler Nguyen <tylerkn1@uci.edu>
2023-11-16 18:27:13 -0800

    added throttle out and brake

--- faultBoard/src/main.cpp
+++ faultBoard/src/main.cpp
@@ -7,21 +7,30 @@ struct can_frame canMsg;
 
 const uint16_t POT_MAX = 1023;
 
-uint8_t ledPin = 3;
+uint8_t MCPin = 3;
+uint8_t throttlePin = A9;
 
-uint16_t pot1 = 0;
-uint16_t pot2 = 0;
+uint16_t throttle1 = 0;
+uint16_t throttle2 = 0;
+uint16_t brake = 0;
 
-uint8_t led = 0;
+uint8_t MC = 0;
 
 MCP2515 mcp2515;
 
-uint32_t faultTime;
+uint32_t faultTime = 0;
+uint8_t throttleOut = 0;
 
+float map_value(uint16_t aMax, uint16_t bMax, uint16_t inValue)
+{
+  // maps value in range a to range b
+  return ((float)bMax / aMax) * (inValue + 0.5);
+}
 
-void setup(){
+void setup()
+{
   Serial.begin(9600);
-  SPI.begin(); //Begins SPI communication
+  SPI.begin(); // Begins SPI communication
 
   mcp2515.init(10);
 
@@ -29,31 +38,43 @@ void setup(){
   mcp2515.setBitrate(CAN_125KBPS);
   mcp2515.setNormalMode();
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, led);
-
+  pinMode(MCPin, OUTPUT);
+  digitalWrite(MCPin, MC);
+  pinMode(throttlePin, OUTPUT);
 }
 
 // FAULT BOARD
-void loop() {
-  if (millis() - faultTime >= 3000){
-    led = 0;
+void loop()
+{
+  if (millis() - faultTime >= 100)
+  {
+    MC = 0;
+    throttleOut = 0;
   }
-  else {
-    led = 1;
+  else
+  {
+    MC = 1;
+    throttleOut = ((throttle1 + throttle2) / 2) / 4;
   }
 
-  if ((((float) pot1)/POT_MAX < 0.05 || ((float) pot2)/POT_MAX < 0.05) || ((float) abs(pot1 - pot2)) / max(pot1, pot2) <= 0.1){
+  if (abs(throttle1 - throttle2) <= POT_MAX / 10)
+  {
     faultTime = millis();
   }
 
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    pot1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1];
-    pot2 = (uint16_t)canMsg.data[2] << 8 | canMsg.data[3];
-    
-    Serial.println("Pot1: " + String(pot1) + " Pot2: " + String(pot2) + " LED: " + String(led));
-  } 
+  if (brake > map_value(5, POT_MAX, 0.5) && brake < map_value(5, POT_MAX, 4.5))
+    MC = 1;
+  else
+    MC = 0;
 
-  digitalWrite(ledPin, led);  
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+  {
+    throttle1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1];
+    throttle2 = (uint16_t)canMsg.data[2] << 8 | canMsg.data[3];
+    brake = (uint16_t)canMsg.data[4] << 8 | canMsg.data[5];
+    Serial.println("throttle1: " + String(throttle1) + " throttle2: " + String(throttle2) + " Brake: " + String(brake) + " MC: " + String(MC) + " Throttle: " + String(throttleOut) + " brake value: " + String(map_value(5, POT_MAX, 4.5)));
+  }
 
+  digitalWrite(MCPin, MC);
+  analogWrite(throttlePin, throttleOut);
 }
--- sensorBoard/src/main.cpp
+++ sensorBoard/src/main.cpp
@@ -3,21 +3,22 @@
 #include <mcp2515.h>
 #include <Arduino_FreeRTOS.h>
 
-
 struct can_frame canMsg;
 
-uint8_t pot1Pin = A7;
-uint8_t pot2Pin = A6;
+uint8_t throttle1Pin = A7;
+uint8_t throttle2Pin = A6;
+uint8_t brakePin = A5;
 
 MCP2515 mcp2515;
 
-void pot1(void *pvParameters);
-void pot2(void *pvParameters);
+void throttle1(void *pvParameters);
+void throttle2(void *pvParameters);
+void brake(void *pvParameters);
 void send(void *pvParameters);
 
-uint16_t pot1Value = 0;
-uint16_t pot2Value = 0;
-
+uint16_t throttle1Value = 0;
+uint16_t throttle2Value = 0;
+uint16_t brakeValue = 0;
 
 void setup()
 {
@@ -30,42 +31,57 @@ void setup()
   mcp2515.setBitrate(CAN_125KBPS);
   mcp2515.setNormalMode();
 
-  xTaskCreate(pot1, "Pot 1", 100, nullptr, 1, nullptr);
-  xTaskCreate(pot2, "Pot 2", 100, nullptr, 1, nullptr);
+  xTaskCreate(throttle1, "Pot 1", 100, nullptr, 1, nullptr);
+  xTaskCreate(throttle2, "Pot 2", 100, nullptr, 1, nullptr);
+  xTaskCreate(brake, "Brake", 100, nullptr, 1, nullptr);
   xTaskCreate(send, "Send", 100, nullptr, 1, nullptr);
   vTaskStartScheduler();
 
-  pinMode(pot1Pin, INPUT);
-  pinMode(pot2Pin, INPUT);
+  pinMode(throttle1Pin, INPUT);
+  pinMode(throttle2Pin, INPUT);
 }
 
 void loop() {}
 
-void pot1(void *pvParameters) {
-  while (true) {
-    pot1Value = analogRead(pot1Pin);
+void throttle1(void *pvParameters)
+{
+  while (true)
+  {
+    throttle1Value = analogRead(throttle1Pin);
   }
 }
 
-void pot2(void *pvParameters) {
-  while (true) {
-    pot2Value = analogRead(pot2Pin);
+void throttle2(void *pvParameters)
+{
+  while (true)
+  {
+    throttle2Value = analogRead(throttle2Pin);
   }
 }
 
-void send(void *pvParameters) {
-  while (true) {
+void brake(void *pvParameteres)
+{
+  while (true)
+  {
+    brakeValue = analogRead(brakePin);
+  }
+}
+
+void send(void *pvParameters)
+{
+  while (true)
+  {
     canMsg.can_id = 0x036; // CAN id as 0x036
     canMsg.can_dlc = 8;    // CAN data length as 8
-    canMsg.data[0] = pot1Value >> 8; 
-    canMsg.data[1] = pot1Value & 0xFF; 
-    canMsg.data[2] = pot2Value >> 8;; 
-    canMsg.data[3] = pot2Value & 0xFF;
-    canMsg.data[4] = 0x05;
-    canMsg.data[5] = 0x06;
+    canMsg.data[0] = throttle1Value >> 8;
+    canMsg.data[1] = throttle1Value & 0xFF;
+    canMsg.data[2] = throttle2Value >> 8;
+    canMsg.data[3] = throttle2Value & 0xFF;
+    canMsg.data[4] = brakeValue >> 8;
+    canMsg.data[5] = brakeValue & 0xFF;
     canMsg.data[6] = 0x07;
     canMsg.data[7] = 0x08;
-  
+
     mcp2515.sendMessage(&canMsg); // Sends the CAN message
     delay(10);
   }
