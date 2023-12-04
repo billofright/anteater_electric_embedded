d751c92fb0ca77e95c5fddff48f34cb1717fda21
billofright <bill.yuan27@gmail.com>
2023-12-03 21:29:00 -0800

    chibios

--- faultBoard/src/main.cpp
+++ faultBoard/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <mcp2515.h>
+#include <ChRt.h>
 
 struct can_frame canMsg;
 
@@ -20,10 +21,40 @@ MCP2515 mcp2515;
 uint32_t faultTime = 0;
 uint8_t throttleOut = 0;
 
+static THD_WORKING_AREA(waThread1, 64);
+
+static THD_FUNCTION(Thread1, arg) {
+  (void)arg;
+  while (true) {
+    chThdSleepMilliseconds(100);
+    Serial.println("Hello");
+  }
+}
+
+static THD_WORKING_AREA(waThread2, 64);
+
+static THD_FUNCTION(Thread2, arg) {
+  (void)arg;
+  while (true) {
+    chThdSleepMilliseconds(100);
+    Serial.println("Hello2");
+  }
+}
+
 float map_value(uint16_t aMax, uint16_t bMax, float inValue)
 {
   // maps value in range a to range b
   return ((float)bMax / aMax) * (inValue);
+}
+
+void chSetup() {
+  // Start threads.
+  chThdCreateStatic(waThread1, sizeof(waThread1),
+    NORMALPRIO, Thread1, NULL);
+
+  chThdCreateStatic(waThread2, sizeof(waThread2),
+    NORMALPRIO, Thread2, NULL);
+
 }
 
 void setup()
@@ -40,40 +71,42 @@ void setup()
   pinMode(MCPin, OUTPUT);
   digitalWrite(MCPin, MC);
   pinMode(throttlePin, OUTPUT);
+
+  chBegin(chSetup);
 }
 
 // FAULT BOARD
 void loop()
 {
-  if (millis() - faultTime >= 100)
-  {
-    MC = 0;
-    throttleOut = 0;
-  }
-  else
-  {
-    MC = 1;
-    throttleOut = ((throttle1 + throttle2) / 2) / 4;
-  }
+  // if (millis() - faultTime >= 100)
+  // {
+  //   MC = 0;
+  //   throttleOut = 0;
+  // }
+  // else
+  // {
+  //   MC = 1;
+  //   throttleOut = ((throttle1 + throttle2) / 2) / 4;
+  // }
 
-  if (abs(throttle1 - throttle2) <= POT_MAX / 10)
-  {
-    faultTime = millis();
-  }
+  // if (abs(throttle1 - throttle2) <= POT_MAX / 10)
+  // {
+  //   faultTime = millis();
+  // }
 
-  if (brake > map_value(5, POT_MAX, 0.5) && brake < map_value(5, POT_MAX, 4.5))
-    MC = 1;
-  else
-    MC = 0;
+  // if (brake > map_value(5, POT_MAX, 0.5) && brake < map_value(5, POT_MAX, 4.5))
+  //   MC = 1;
+  // else
+  //   MC = 0;
 
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
-  {
-    throttle1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1];
-    throttle2 = (uint16_t)canMsg.data[2] << 8 | canMsg.data[3];
-    brake = (uint16_t)canMsg.data[4] << 8 | canMsg.data[5];
-    Serial.println("throttle1: " + String(throttle1) + " throttle2: " + String(throttle2) + " Brake: " + String(brake) + " MC: " + String(MC) + " Throttle: " + String(throttleOut) + " brake value: " + String(map_value(5, POT_MAX, 4.5)));
-  }
+  // if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK)
+  // {
+  //   throttle1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1];
+  //   throttle2 = (uint16_t)canMsg.data[2] << 8 | canMsg.data[3];
+  //   brake = (uint16_t)canMsg.data[4] << 8 | canMsg.data[5];
+  //   Serial.println("throttle1: " + String(throttle1) + " throttle2: " + String(throttle2) + " Brake: " + String(brake) + " MC: " + String(MC) + " Throttle: " + String(throttleOut) + " brake value: " + String(map_value(5, POT_MAX, 4.5)));
+  // }
 
-  digitalWrite(MCPin, MC);
-  analogWrite(throttlePin, throttleOut);
+  // digitalWrite(MCPin, MC);
+  // analogWrite(throttlePin, throttleOut);
 }
