9a9a2680653de03947d9ff655c74c0e948ade761
billofright <bill.yuan27@gmail.com>
2023-12-04 21:01:55 -0800

    state cycling kinda working but keeps freezing

--- faultBoard/src/main.cpp
+++ faultBoard/src/main.cpp
@@ -12,7 +12,11 @@ enum State
   FAULT
 };
 
+String stateNames[] = {"IDLE", "TRACTIVE_SYSTEM_ACTIVE", "READY_TO_DRIVE", "DRIVING", "FAULT"};
+
 MUTEX_DECL(stateMutex);
+MUTEX_DECL(appsMutex);
+MUTEX_DECL(bseMutex);
 
 struct can_frame canMsg;
 
@@ -22,11 +26,11 @@ uint8_t MCPin = 3;
 uint8_t throttlePin = A9;
 uint8_t buzzerPin = 4;
 
-uint8_t tractiveSystemActiveValue = 0;
-uint8_t keySwitchValue = 0;
 uint16_t throttle1 = 0;
 uint16_t throttle2 = 0;
 uint16_t brake = 0;
+uint8_t tractiveSystemActiveValue = 0;
+uint8_t keySwitchValue = 0;
 
 float brake_low = 0.5;
 float brake_high = 4.5;
@@ -38,7 +42,11 @@ MCP2515 mcp2515;
 uint32_t faultTime = 0;
 uint8_t throttleOut = 0;
 
-uint8_t CURR_STATE = IDLE;
+State CURR_STATE = IDLE;
+
+uint8_t appsFault = 0;
+uint8_t bseFault = 0;
+
 
 static THD_WORKING_AREA(waThread1, 64);
 
@@ -47,25 +55,21 @@ static THD_FUNCTION(throttleCheck, arg)
   (void)arg;
   while (true)
   {
-    chMtxLock(&stateMutex);
-    if (millis() - faultTime >= 100)
-    {
-      MC = 0;
-      throttleOut = 0;
-      if (CURR_STATE != FAULT)
-      {
-        CURR_STATE = FAULT;
-      }
-    }
-    else
-    {
-      MC = 1;
-      throttleOut = ((throttle1 + throttle2) / 2) / 4;
-    }
-    chMtxUnlock(&stateMutex);
     if (abs(throttle1 - throttle2) <= POT_MAX / 10)
     {
       faultTime = millis();
+    }
+    if (millis() - faultTime >= 100)
+    {
+      chMtxLock(&appsMutex);
+      appsFault = true;
+      chMtxUnlock(&appsMutex);
+    }
+    else
+    {
+      chMtxLock(&appsMutex);
+      appsFault = false;
+      chMtxUnlock(&appsMutex);
     }
   }
 }
@@ -83,20 +87,10 @@ static THD_FUNCTION(brakeCheck, arg)
   (void)arg;
   while (true)
   {
-    if (brake > map_value(5, POT_MAX, 0.5) && brake < map_value(5, POT_MAX, 4.5))
-    {
-      MC = 1;
-    }
-    else
-    {
-      MC = 0;
-      chMtxLock(&stateMutex);
-      if (CURR_STATE != FAULT)
-      {
-        CURR_STATE = FAULT;
-      }
-      chMtxUnlock(&stateMutex);
-    }
+    float brakeVal = map_value(POT_MAX, 5, brake);
+    chMtxLock(&bseMutex);
+    bseFault = brakeVal < 0.5 || brakeVal > 4.5;
+    chMtxUnlock(&bseMutex);
   }
 }
 
@@ -107,6 +101,11 @@ static THD_FUNCTION(rtd, arg)
   (void)arg;
   while (true)
   {
+    // tone(buzzerPin, freq);
+    // freq += 500;
+
+    // Serial.println(freq);
+    // chThdSleepMilliseconds(10);
     chMtxLock(&stateMutex);
 
     if (CURR_STATE == IDLE)
@@ -114,16 +113,14 @@ static THD_FUNCTION(rtd, arg)
       if (tractiveSystemActiveValue == 1)
       {
         CURR_STATE = TRACTIVE_SYSTEM_ACTIVE;
-        Serial.println("Tractive system active!");
       }
     }
     else if (CURR_STATE == TRACTIVE_SYSTEM_ACTIVE)
     {
-      if (brake > brake_low && brake < brake_high)
+      if (map_value(POT_MAX, 5, brake) > brake_low && map_value(POT_MAX, 5, brake) < brake_high)
       {
         CURR_STATE = READY_TO_DRIVE;
-        Serial.println("Ready to drive sound playing!");
-        tone(buzzerPin, 1000, 1000);
+        tone(buzzerPin, 1000, 3000);
       }
     }
     else if (CURR_STATE == READY_TO_DRIVE)
@@ -131,7 +128,6 @@ static THD_FUNCTION(rtd, arg)
       if (keySwitchValue == 1)
       {
         CURR_STATE = DRIVING;
-        Serial.println("Driving!");
       }
     }
 
@@ -151,9 +147,26 @@ static THD_FUNCTION(read, arg)
       throttle1 = (uint16_t)canMsg.data[0] << 8 | canMsg.data[1];
       throttle2 = (uint16_t)canMsg.data[2] << 8 | canMsg.data[3];
       brake = (uint16_t)canMsg.data[4] << 8 | canMsg.data[5];
-      tractiveSystemActiveValue = (uint16_t)canMsg.data[6];
-      keySwitchValue = (uint16_t)canMsg.data[7];
-      Serial.println("throttle1: " + String(throttle1) + " throttle2: " + String(throttle2) + " Brake: " + String(brake) + " MC: " + String(MC) + " Throttle: " + String(throttleOut) + " brake value: " + String(map_value(5, POT_MAX, 4.5)));
+      tractiveSystemActiveValue = canMsg.data[6];
+      keySwitchValue = canMsg.data[7];
+      Serial.print("throttle1: ");
+      Serial.print(throttle1);
+      Serial.print(" throttle2: ");
+      Serial.print(throttle2);
+      Serial.print(" Brake: ");
+      Serial.print(brake);
+      Serial.print(" brake value: ");
+      Serial.print(map_value(POT_MAX, 5, brake));
+      Serial.print(" MC: ");
+      Serial.print(MC);
+      Serial.print(" Throttle: ");
+      Serial.print(throttleOut);
+      Serial.print(" tractiveSystemActiveValue: ");
+      Serial.print(tractiveSystemActiveValue);
+      Serial.print(" keySwitchValue: ");
+      Serial.print(keySwitchValue);
+      Serial.print(" current state: ");
+      Serial.println(stateNames[CURR_STATE]);
     }
   }
 }
@@ -165,6 +178,35 @@ static THD_FUNCTION(write, arg)
   (void)arg;
   while (true)
   {
+    chMtxLock(&bseMutex);
+    uint8_t currBse = bseFault;
+    chMtxUnlock(&bseMutex);
+
+    chMtxLock(&appsMutex);
+    uint8_t currApps = appsFault;
+    chMtxUnlock(&appsMutex);
+
+    if(currBse || currApps)
+    {
+      chMtxLock(&stateMutex);
+      if(CURR_STATE == DRIVING){
+        CURR_STATE = FAULT;
+      }
+      chMtxUnlock(&stateMutex);
+      MC = 0;
+      throttleOut = 0;
+    }
+    else
+    {
+      chMtxLock(&stateMutex);
+      if(CURR_STATE == FAULT){
+        CURR_STATE = DRIVING;
+      }
+      chMtxUnlock(&stateMutex);
+      MC = 1;
+      throttleOut = ((throttle1 + throttle2) / 2) / 4;
+    }
+
     digitalWrite(MCPin, MC);
     analogWrite(throttlePin, throttleOut);
   }
